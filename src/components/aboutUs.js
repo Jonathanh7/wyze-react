5cfa50b8566ae420d468f89ff509087502c2b39c
Jonathanh7 <Jonathanh949@gmail.com>
2020-02-27 16:50:28 -0500

    done

--- src/components/aboutUs.js
+++ src/components/aboutUs.js
@@ -4,12 +4,22 @@ export default function aboutUs() {
   return (
     <div>
       <h1>About Us</h1>
+      <div className="Summary">
+        <p>
+          Here at Wyze, we like to give back to humanity.</p>
+        
+        <p>
+           Not in material
+        goods you see, but in great wisedom. We all need a spark to our day,</p>
+
       <p>
-        Here at Wyze, we like to give back to humanity. But not in material
-        goods you see, but in great wisedom. We all need a spark to our day, an
-        idea that lights the bulb. Do enjoy this website as a token to humanity.
-        Sincerely, Wyze
+         An
+        idea that lights the bulb.</p>
+        
+        <p> Do enjoy this website as a token to humanity.
       </p>
+        <p>Sincerely Wyze</p>
+        </div>
     </div>
   );
 }
